5a4d5863620eb18441ee51d896247fc2c2497f77
Jorge-GustavoAlencarOliveira <jorgegustavo500@gmail.com>
2023-03-25 21:44:33 -0300

    6

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,4 +1,4 @@
 import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({ datasources: {  db: { url: "postgresql://postgres:admin@localhost:5432/pizzaria?schema=public" } } })
 
 export default prisma;
